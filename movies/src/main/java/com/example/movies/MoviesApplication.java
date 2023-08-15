6e4d0b50495399361cde4d4c5e2a87606893d745
shoaib <shoaibhossain24@gmail.com>
2023-08-15 15:56:45 +0600

    Add till working of database with app

--- movies/src/main/java/com/example/movies/MoviesApplication.java
+++ movies/src/main/java/com/example/movies/MoviesApplication.java
@@ -6,18 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-	//The @GetMapping works like the post from express app
-	@GetMapping("/root")
-	//The apiRoute method works like the post method
-	public String apiRoute(){
-		return  "Hello ,World!";
-	}
+
 
 }
