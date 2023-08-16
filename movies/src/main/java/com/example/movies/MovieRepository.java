739ec98ab87471a50d6b40f80e2b1562ba691adb
shoaib <shoaibhossain24@gmail.com>
2023-08-16 07:56:16 +0600

    Final commit

--- movies/src/main/java/com/example/movies/MovieRepository.java
+++ movies/src/main/java/com/example/movies/MovieRepository.java
@@ -1,11 +1,17 @@
 package com.example.movies;
 
 import org.bson.types.ObjectId;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
+
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 
+    Optional<Movie> findMovieByImdbId(String imdbId);
 
 }
--- movies/src/main/java/com/example/movies/MovieService.java
+++ movies/src/main/java/com/example/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.example.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +17,7 @@ public class MovieService {
         return movieRepository.findAll();
 
     }
-
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
 }
